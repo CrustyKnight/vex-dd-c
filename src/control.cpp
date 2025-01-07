a3fc994070caf6a928af3f9bfe7e6968b05d71d4
Cole T. <coledt54@gmail.com>
2025-01-07 16:10:36 -0700

    formatting

--- src/control.cpp
+++ src/control.cpp
@@ -1,7 +1,6 @@
-#include "main.h"
-#include "lemlib/api.hpp"
 #include "api.h"
-
+#include "lemlib/api.hpp"
+#include "main.h"
 
 #define INTAKE_BUTTON DIGITAL_R1
 #define INTAKE_REVERSE_BUTTON DIGITAL_R2
@@ -15,7 +14,7 @@ pros::Motor intake_motor(7, pros::MotorGearset::green);
 pros::adi::DigitalOut clamp_piston_a('H');
 pros::Motor ext(8, pros::MotorGearset::red);
 
-//pros::adi::DigitalOut clamp_piston_b('B');
+// pros::adi::DigitalOut clamp_piston_b('B');
 
 /*
 #define DIGITAL_L1 pros::E_CONTROLLER_DIGITAL_L1
@@ -33,16 +32,16 @@ pros::Motor ext(8, pros::MotorGearset::red);
 */
 
 class Button {
-  public:
-    pros::controller_digital_e_t button;
-    Button(pros::controller_digital_e_t);
-    int previous_state;
-    int current_state;
-    void update(pros::Controller);
-    bool just_pressed();
-    bool held() { return current_state == 1;};
+ public:
+  pros::controller_digital_e_t button;
+  Button(pros::controller_digital_e_t);
+  int previous_state;
+  int current_state;
+  void update(pros::Controller);
+  bool just_pressed();
+  bool held() { return current_state == 1; };
 };
-Button::Button (pros::controller_digital_e_t b) {
+Button::Button(pros::controller_digital_e_t b) {
   button = b;
   current_state = 0;
 }
@@ -59,12 +58,11 @@ Button intake_reverse_button(INTAKE_REVERSE_BUTTON);
 Button clamp_up_button(CLAMP_UP_BUTTON);
 Button clamp_down_button(CLAMP_DOWN_BUTTON);
 
-int intake_on_power = -100; // basically, how fast should the intake run when its on
+int intake_on_power = -100;  // basically, how fast should the intake run when its on
 // 50 is a random guess
-bool intake_on_state = false; // true if on, false if off.
+bool intake_on_state = false;  // true if on, false if off.
 
 int ext_on = -100;
-
 
 // Set the motor power for the intake
 void set_intake_power(int8_t power) {
@@ -88,18 +86,17 @@ void intake_off() {
   intake_motor.move(0);
 }
 
-
 // Intake button R1
 
-void drive_intake_hold(pros::Controller master){
-   intake_button.update(master);
-   intake_reverse_button.update(master);
-   int intake_state = intake_button.current_state;
-   int intake_backward_state = intake_reverse_button.current_state;
-   /*
-	int intake_state = master.get_digital(INTAKE_BUTTON);
-  	int intake_backward_state = master.get_digital(INTAKE_REVERSE_BUTTON);
-	*/ 
+void drive_intake_hold(pros::Controller master) {
+  intake_button.update(master);
+  intake_reverse_button.update(master);
+  int intake_state = intake_button.current_state;
+  int intake_backward_state = intake_reverse_button.current_state;
+  /*
+ int intake_state = master.get_digital(INTAKE_BUTTON);
+   int intake_backward_state = master.get_digital(INTAKE_REVERSE_BUTTON);
+ */
   if (intake_state == 1) {
     set_intake_power(100);
     intake_on();
@@ -111,12 +108,11 @@ void drive_intake_hold(pros::Controller master){
   }
 }
 
-
 int previous_B_state = 0;
 
 void drive_intake_toggle(pros::Controller master) {
   int intake_state = master.get_digital(pros::E_CONTROLLER_DIGITAL_B);
-  if (intake_state != previous_B_state) { // change
+  if (intake_state != previous_B_state) {  // change
     if (intake_state == 1) {
       intake_toggle();
     }
@@ -128,91 +124,85 @@ void drive_intake(pros::Controller master) {
   drive_intake_hold(master);
 }
 
-
 int level = 0;
 int prev_forward = 0;
 int prev_reverse = 0;
 int going = 0;
-void drive_extend(pros::Controller master)
-{
-	ext.set_encoder_units(pros::E_MOTOR_ENCODER_ROTATIONS);
-	int forward = master.get_digital(pros::E_CONTROLLER_DIGITAL_UP);
-	int reverse = master.get_digital(pros::E_CONTROLLER_DIGITAL_DOWN);
+void drive_extend(pros::Controller master) {
+  ext.set_encoder_units(pros::E_MOTOR_ENCODER_ROTATIONS);
+  int forward = master.get_digital(pros::E_CONTROLLER_DIGITAL_UP);
+  int reverse = master.get_digital(pros::E_CONTROLLER_DIGITAL_DOWN);
 
-	if (std::floor(ext.get_actual_velocity()) == 0){
-		going = 0;
-	}
-	else{
-		going = 1;
-	}
-	
-	if ((going == 1) || (forward == prev_forward && reverse == prev_reverse) /*|| (level == 2 && forward == 1) || (level == 0 && reverse == 1)*/){
-		return;
-	}
-	double unit = 19.0/12.0;
+  if (std::floor(ext.get_actual_velocity()) == 0) {
+    going = 0;
+  } else {
+    going = 1;
+  }
 
+  if ((going == 1) || (forward == prev_forward && reverse == prev_reverse) /*|| (level == 2 && forward == 1) || (level == 0 && reverse == 1)*/) {
+    return;
+  }
+  double unit = 19.0 / 12.0;
 
-	if (forward == 1){
-		level--;
-		ext.move_relative(7*unit, 100);
-		going = 1;
-	}
-	else if (reverse == 1){
-		level++;
-		ext.move_relative(-7*unit, 100);
-		going = 1;
-	}
-	prev_forward = forward;
-	prev_reverse = reverse;
-
-
+  if (forward == 1) {
+    level--;
+    ext.move_relative(7 * unit, 100);
+    going = 1;
+  } else if (reverse == 1) {
+    level++;
+    ext.move_relative(-7 * unit, 100);
+    going = 1;
+  }
+  prev_forward = forward;
+  prev_reverse = reverse;
 }
-
 
 bool clamp_on_state = false;
 
 void clamp_pistons_set(int value) {
-    clamp_piston_a.set_value(value);
-    //clamp_piston_b.set_value(value);
+  clamp_piston_a.set_value(value);
+  // clamp_piston_b.set_value(value);
 }
 
 void clamp_engage() {
-    clamp_on_state = true;
-    clamp_pistons_set(CLAMP_ON);
+  clamp_on_state = true;
+  clamp_pistons_set(CLAMP_ON);
 }
 
 void clamp_disengage() {
-    clamp_on_state = false;
-    clamp_pistons_set(CLAMP_OFF);
+  clamp_on_state = false;
+  clamp_pistons_set(CLAMP_OFF);
 }
 
-void clamp_toggle () {
-    if (clamp_on_state) {
-        clamp_disengage();
-    } else {
-        clamp_engage();
-    }
+void clamp_toggle() {
+  if (clamp_on_state) {
+    clamp_disengage();
+  } else {
+    clamp_engage();
+  }
 }
 
 int previous_clamp_button_state = 0;
 
 void drive_clamp_toggle(pros::Controller master) {
-    int clamp_state = master.get_digital(CLAMP_BUTTON);
-    if (clamp_state != previous_clamp_button_state) {
-        if(clamp_state == 1) {
-            clamp_toggle();
-        }
+  int clamp_state = master.get_digital(CLAMP_BUTTON);
+  if (clamp_state != previous_clamp_button_state) {
+    if (clamp_state == 1) {
+      clamp_toggle();
     }
-    previous_clamp_button_state = clamp_state;
+  }
+  previous_clamp_button_state = clamp_state;
 }
 
 int previous_clamp_up_button_state = 0;
 int previous_clamp_down_button_state = 0;
 void drive_clamp_up_down(pros::Controller master) {
-   clamp_up_button.update(master);
-   clamp_down_button.update(master);
-   if (clamp_up_button.just_pressed()) clamp_disengage();
-   else if (clamp_down_button.just_pressed()) clamp_engage();
+  clamp_up_button.update(master);
+  clamp_down_button.update(master);
+  if (clamp_up_button.just_pressed())
+    clamp_disengage();
+  else if (clamp_down_button.just_pressed())
+    clamp_engage();
 }
 
 void drive_clamp(pros::Controller master) {
--- src/display.cpp
+++ src/display.cpp
@@ -1,13 +1,12 @@
-#include "main.h"
-#include "pros/apix.h"
-
 #include <string>
 
 #include "liblvgl/lvgl.h"
+#include "main.h"
+#include "pros/apix.h"
 
 // catpuccin mocha B)
 #define cat_blue 0x89b4fa
-#define cat_red 0xf38ba8 
+#define cat_red 0xf38ba8
 #define cat_green 0xa65e3a1
 #define cat_yellow 0xf9e2af
 #define cat_orange 0xfab387
@@ -21,155 +20,139 @@
 static lv_style_t style_btn;
 static lv_style_t style_button_pressed;
 
-lv_obj_t * diag;
-lv_obj_t * table;
+lv_obj_t* diag;
+lv_obj_t* table;
 
+static void test_button(lv_event_t* e) {
+  lv_event_code_t code = lv_event_get_code(e);
+  lv_obj_t* button = lv_event_get_target(e);
+  if (code == LV_EVENT_CLICKED) {
+    static uint8_t cnt = 0;
+    cnt++;
 
-static void test_button(lv_event_t * e){
-    lv_event_code_t code = lv_event_get_code(e);
-    lv_obj_t * button = lv_event_get_target(e);
-    if(code == LV_EVENT_CLICKED) {
-      static uint8_t cnt = 0;
-      cnt++;
-
-
-      lv_obj_t * label = lv_obj_get_child(button,0);
-      lv_label_set_text_fmt(label, "Pressed %d times", cnt);
-    }
+    lv_obj_t* label = lv_obj_get_child(button, 0);
+    lv_label_set_text_fmt(label, "Pressed %d times", cnt);
+  }
 }
 
-static lv_color_t darken(const lv_color_filter_dsc_t * dsc, lv_color_t color, lv_opa_t opa)
-{
-    LV_UNUSED(dsc);
-    return lv_color_darken(color, opa);
+static lv_color_t darken(const lv_color_filter_dsc_t* dsc, lv_color_t color, lv_opa_t opa) {
+  LV_UNUSED(dsc);
+  return lv_color_darken(color, opa);
 }
 
 LV_IMG_DECLARE(catplush);
-static lv_obj_t * catplush_img;
-static lv_obj_t * maint;
+static lv_obj_t* catplush_img;
+static lv_obj_t* maint;
 
-lv_obj_t * motortemps;
+lv_obj_t* motortemps;
 
-void draw_catplush(){
+void draw_catplush() {
   catplush_img = lv_img_create(maint);
   lv_img_set_src(catplush_img, &catplush);
   lv_obj_align(catplush_img, LV_ALIGN_CENTER, 0, -30);
   lv_img_set_zoom(catplush_img, 512);
 }
 
-static void style_init(void){
+static void style_init(void) {
+  // BUTTON
+  lv_style_init(&style_btn);
+  lv_style_set_radius(&style_btn, 10);
+  lv_style_set_border_color(&style_btn, lv_color_hex(cat_lavender));
+  lv_style_set_bg_opa(&style_btn, 255);
+  lv_style_set_bg_color(&style_btn, lv_color_hex(cat_lavender));
+  lv_style_set_border_width(&style_btn, 2);
+  lv_style_set_text_color(&style_btn, lv_color_hex(cat_crust));
 
-  //BUTTON
-    lv_style_init(&style_btn);
-    lv_style_set_radius(&style_btn, 10);
-    lv_style_set_border_color(&style_btn, lv_color_hex(cat_lavender));
-    lv_style_set_bg_opa(&style_btn, 255);
-    lv_style_set_bg_color(&style_btn, lv_color_hex(cat_lavender));
-    lv_style_set_border_width(&style_btn, 2);
-    lv_style_set_text_color(&style_btn, lv_color_hex(cat_crust));
-
-  //BUTTON PRESSED
-    static lv_color_filter_dsc_t color_filter;
-    lv_color_filter_dsc_init(&color_filter, darken);
-    lv_style_init(&style_button_pressed);
-    lv_style_set_color_filter_dsc(&style_button_pressed, &color_filter);
-    lv_style_set_color_filter_opa(&style_button_pressed, LV_OPA_20);
-
+  // BUTTON PRESSED
+  static lv_color_filter_dsc_t color_filter;
+  lv_color_filter_dsc_init(&color_filter, darken);
+  lv_style_init(&style_button_pressed);
+  lv_style_set_color_filter_dsc(&style_button_pressed, &color_filter);
+  lv_style_set_color_filter_opa(&style_button_pressed, LV_OPA_20);
 }
 
-void telemetry_table(lv_obj_t * tab){
-
+void telemetry_table(lv_obj_t* tab) {
   table = lv_table_create(tab);
 
   lv_table_set_col_width(table, 0, 90);
   lv_obj_set_style_pad_ver(table, 10, LV_PART_ITEMS);
 
-  lv_table_set_cell_value(table, 0,0,"motor");
-  lv_table_set_cell_value(table, 0,1,"temp");
+  lv_table_set_cell_value(table, 0, 0, "motor");
+  lv_table_set_cell_value(table, 0, 1, "temp");
 
-
-  //scuffed
+  // scuffed
   std::vector<double> mt = get_motor_temps();
-  std::vector<int> mtemps; 
+  std::vector<int> mtemps;
 
   std::vector<double> tqs = get_motor_torques();
   std::vector<int> torques;
 
   std::vector<double> rs = get_motor_rpms();
   std::vector<int> rpms;
-  for(int i = 0; i < 6; i++){
-    mtemps.push_back((int) mt[i]);
-    torques.push_back((int) tqs[i]);
-    rpms.push_back((int) rs[i]);
+  for (int i = 0; i < 6; i++) {
+    mtemps.push_back((int)mt[i]);
+    torques.push_back((int)tqs[i]);
+    rpms.push_back((int)rs[i]);
   }
 
-
-  for(int i = 1; i < 7; i++){
+  for (int i = 1; i < 7; i++) {
     lv_table_set_cell_value_fmt(table, i, 0, "%i", i);
-    lv_table_set_cell_value_fmt(table, i, 1, "%f", mtemps[i-1]);
-    lv_table_set_cell_value_fmt(table, i, 2, "%f", torques[i-1]);
-    lv_table_set_cell_value_fmt(table, i, 3, "%f", rpms[i-1]);
+    lv_table_set_cell_value_fmt(table, i, 1, "%f", mtemps[i - 1]);
+    lv_table_set_cell_value_fmt(table, i, 2, "%f", torques[i - 1]);
+    lv_table_set_cell_value_fmt(table, i, 3, "%f", rpms[i - 1]);
   }
-
 
   lv_obj_align(table, LV_DIR_LEFT, -25, -25);
-
 }
 
+lv_obj_t* title;
 
-
-lv_obj_t * title;
-
-void display_init(void){
-
+void display_init(void) {
   style_init();
 
-  lv_obj_t * tabview;
+  lv_obj_t* tabview;
   tabview = lv_tabview_create(lv_scr_act(), LV_DIR_TOP, 30);
 
-    lv_obj_t * tab_btns = lv_tabview_get_tab_btns(tabview);
-    lv_obj_set_style_bg_color(tab_btns, lv_palette_darken(LV_PALETTE_GREY, 3), 0);
-    lv_obj_set_style_text_color(tab_btns, lv_palette_lighten(LV_PALETTE_GREY, 5), 0);
-    lv_obj_set_style_border_side(tab_btns, LV_BORDER_SIDE_RIGHT, LV_PART_ITEMS | LV_STATE_CHECKED);
+  lv_obj_t* tab_btns = lv_tabview_get_tab_btns(tabview);
+  lv_obj_set_style_bg_color(tab_btns, lv_palette_darken(LV_PALETTE_GREY, 3), 0);
+  lv_obj_set_style_text_color(tab_btns, lv_palette_lighten(LV_PALETTE_GREY, 5), 0);
+  lv_obj_set_style_border_side(tab_btns, LV_BORDER_SIDE_RIGHT, LV_PART_ITEMS | LV_STATE_CHECKED);
 
   lv_obj_set_style_bg_color(lv_scr_act(), lv_color_hex(cat_crust), LV_PART_MAIN);
 
-    maint = lv_tabview_add_tab(tabview, "MAIN");
-    diag = lv_tabview_add_tab(tabview, "INFO");
- 
-    title = lv_label_create(maint);
-    lv_label_set_text(title, "~~DONUT DYNAMICS~~");
-    lv_obj_set_style_text_color(maint, lv_color_hex(TEXTCLR), LV_PART_MAIN);
-    
-    draw_catplush();
+  maint = lv_tabview_add_tab(tabview, "MAIN");
+  diag = lv_tabview_add_tab(tabview, "INFO");
 
-/*
-    lv_obj_t * testbutton = lv_btn_create(maint); //create a button as a child of the main display (screen)
-    lv_obj_remove_style_all(testbutton);
-    lv_obj_add_style(testbutton, &style_btn, 0);
-    lv_obj_set_pos(testbutton, 175, 50); //0,0 is in the topleft corner
-    lv_obj_set_size(testbutton, 120, 50);
-    lv_obj_add_event_cb(testbutton, test_button, LV_EVENT_ALL, NULL);           //Assign a callback to the button
-    lv_obj_add_style(testbutton, &style_button_pressed, LV_STATE_PRESSED);
+  title = lv_label_create(maint);
+  lv_label_set_text(title, "~~DONUT DYNAMICS~~");
+  lv_obj_set_style_text_color(maint, lv_color_hex(TEXTCLR), LV_PART_MAIN);
 
-    lv_obj_t * testbtnlabel = lv_label_create(testbutton); //label for the button
-    lv_label_set_text(testbtnlabel, "press me?");
-    lv_obj_center(testbtnlabel);
-*/
+  draw_catplush();
 
-    telemetry_table(diag);
+  /*
+      lv_obj_t * testbutton = lv_btn_create(maint); //create a button as a child of the main display (screen)
+      lv_obj_remove_style_all(testbutton);
+      lv_obj_add_style(testbutton, &style_btn, 0);
+      lv_obj_set_pos(testbutton, 175, 50); //0,0 is in the topleft corner
+      lv_obj_set_size(testbutton, 120, 50);
+      lv_obj_add_event_cb(testbutton, test_button, LV_EVENT_ALL, NULL);           //Assign a callback to the button
+      lv_obj_add_style(testbutton, &style_button_pressed, LV_STATE_PRESSED);
 
-    /*
-    motortemps = lv_label_create(diag);
-    lv_obj_align(motortemps, LV_DIR_LEFT, 0, 0);
-    std::vector<double> mtemps = get_motor_temps();
-    lv_label_set_text_fmt(motortemps, "MOTOR 1: %d\nMOTOR 2: %d\nMOTOR 3: %d\nMOTOR 4: %d\nMOTOR 5: %d\nMOTOR 6: %d", mtemps[0], mtemps[1], mtemps[2], mtemps[3], mtemps[4], mtemps[5]);
-    */
+      lv_obj_t * testbtnlabel = lv_label_create(testbutton); //label for the button
+      lv_label_set_text(testbtnlabel, "press me?");
+      lv_obj_center(testbtnlabel);
+  */
 
+  telemetry_table(diag);
 
-
+  /*
+  motortemps = lv_label_create(diag);
+  lv_obj_align(motortemps, LV_DIR_LEFT, 0, 0);
+  std::vector<double> mtemps = get_motor_temps();
+  lv_label_set_text_fmt(motortemps, "MOTOR 1: %d\nMOTOR 2: %d\nMOTOR 3: %d\nMOTOR 4: %d\nMOTOR 5: %d\nMOTOR 6: %d", mtemps[0], mtemps[1], mtemps[2], mtemps[3], mtemps[4], mtemps[5]);
+  */
 }
 
-void display_tick(void){
-  //telemetry_table(diag);
+void display_tick(void) {
+  // telemetry_table(diag);
 }
