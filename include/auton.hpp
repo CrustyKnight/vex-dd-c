caf462160a2f83515670a97cb9429067668e500e
Sam Cormode <samcormode@gmail.com>
2025-01-14 16:41:01 -0700

    fix for chassis not being able to be directly passed around

--- include/auton.hpp
+++ include/auton.hpp
@@ -2,7 +2,7 @@
 #include "lemlib/api.hpp"
 #include <string>
 
-void autonSkills();
-//void gameAuton(string startLocation);
+void autonSkills(lemlib::Chassis*);
+// void gameAuton(string startLocation);
 void defensiveAuton();
 void offensiveAuton();
--- src/auton.cpp
+++ src/auton.cpp
@@ -1,3 +1,4 @@
+#include "lemlib/api.hpp"
 #include "auton.hpp"
 #include "clamp.hpp"
 #include "intake.hpp"
@@ -9,18 +10,17 @@
 #define SKILLS_Y_START 23
 #define SKILLS_THETA_START 180
 
-static void get_mogo(lemlib::Chassis chassis, double x, double y) {
-	chassis.moveToPoint(x, y, 5000, {.forwards = false}, false);
-	clamp::engage();
+static void get_mogo(lemlib::Chassis* chassis, double x, double y) {
+  chassis->moveToPoint(x, y, 5000, {.forwards = false}, false);
+  clamp::engage();
 }
 
 //#TODO: create efficient skills plan...
-void autonSkills(lemlib::Chassis chassis) {
-  chassis.setPose(SKILLS_X_START, SKILLS_Y_START, SKILLS_THETA_START);
-	get_mogo(chassis, -54, 23.5);
-	chassis.moveToPose(-58.3, 47.0, 90, 5000);
-	intake::eat_donut();	
-	
+void autonSkills(lemlib::Chassis* chassis) {
+  chassis->setPose(SKILLS_X_START, SKILLS_Y_START, SKILLS_THETA_START);
+  get_mogo(chassis, -54, 23.5);
+  chassis->moveToPose(-58.3, 47.0, 90, 5000);
+  intake::eat_donut();
 }
 
 //#TODO: create game plan...
